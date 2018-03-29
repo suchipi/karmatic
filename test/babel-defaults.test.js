e96f07a32665829709cb79fba357d7c9587b2e6b
Suchipi Izumi <me@suchipi.com>
2018-03-28 21:47:46 -0600

    eslint fix

--- test/babel-defaults.test.js
+++ test/babel-defaults.test.js
@@ -1,7 +1,7 @@
 it("should use the stage-0 preset out of the box", () => {
   // should not fail to parse
+  /* eslint-disable */
   function newSyntax() {
-    /* eslint-disable */
     foo::bar();
     const { abc } = { ...oneTwoThree };
     return new.target;
